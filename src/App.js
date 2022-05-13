171a3359563535ea39b94d7b8c02b17b5bd283ba
madesurya <madesurya27@gmail.com>
2022-05-13 18:31:05 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchMain from './components/SearchMain';
+
+function App() {
+  return (  
+  <div className="App">
+    <SearchMain/>
+  </div>
+  );
+}
+
+export default App;
